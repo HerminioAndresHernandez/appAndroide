99271c1369981f71bc2d0d56db7a1cc8ee1af966
Ignacio <ignacio.a.r@hotmail.com>
2017-12-10 12:58:13 -0300

    Agregado servicio en mainActivity

--- app/src/main/java/com/example/her_1/afinal/MainActivity.java
+++ app/src/main/java/com/example/her_1/afinal/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,14 +18,18 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -34,18 +39,21 @@ public class MainActivity extends AppCompatActivity {
     public static final int GET_RESULTM = 3;
     public static final int GET_RESULTC = 4;
 
+    private static final String TAG =  MainActivity.class.getCanonicalName();
+
     public static final String fecha = null;
     private int counter = 0;
     private boolean active = false;
     public DatosPartido dato;
-    OkHttpClient client = new OkHttpClient();
+
 
     /**Check internet conection*/
     public static ConnectivityManager connMgr;
     public static NetworkInfo networkInfo;
     public static ArrayList<String> datos;
 
-
+    OkHttpClient client = new OkHttpClient();
+    Request request;
 
     Intent intentAyer;
     Intent intentHoy;
@@ -68,7 +76,25 @@ public class MainActivity extends AppCompatActivity {
         intentManana = new Intent(MainActivity.this, MainActivityManana.class);
         intentManana.putStringArrayListExtra("datos", new ArrayList<String>());
 
-        Request request = new Request.Builder().url("http://publicobject.com/helloworld.txt").build();
+        request = new Request.Builder().url("http://192.168.0.3:8080/OlimpicRestServer/olimpic/getpartidos").build();
+
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                Log.d(TAG, "NO ANDAAAAAAAAAAAAAAAAAAAAAAA");
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                try {
+                    String responseData = response.body().string();
+                    jsonjArray = new JSONArray(responseData);
+                    Log.d(TAG, "ANDAAAAAAAAAAAAAAAAAAAAAAA33333333");
+                } catch (JSONException e) {Log.d(TAG, "NO ANDAAAAAAAAAAAAAAAAAAAAAAA2");}
+            }
+        });
 
         /**Check internet conection*/
         connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -116,13 +142,7 @@ public class MainActivity extends AppCompatActivity {
             AsyncTask<?, ?, ?> asyncTask = new AsyncTask<Object, DatosPartido, Object>() {
                 @Override
                 protected Object doInBackground(Object... params) {
-                    //while (true) {
-                        try {
-                            /**Reemplazar por el servicio*/
-                            jsonjArray = new JSONArray(getPartidos());
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+
                         while (!active) {
                             synchronized (MainActivity.this) {
                                 try {
@@ -132,9 +152,11 @@ public class MainActivity extends AppCompatActivity {
                                 }
                             }
                         }
+
                     intentAyer.getStringArrayListExtra("datos").clear();
                     intentHoy.getStringArrayListExtra("datos").clear();
                     intentManana.getStringArrayListExtra("datos").clear();
+
                     for (int i = 0; i < jsonjArray.length(); i++) {
                         synchronized (MainActivity.this) {
                             try {
