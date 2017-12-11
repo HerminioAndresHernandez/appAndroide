4ba2f52d8cdb60d194b92fa65f69ba8a790bfa50
Ignacio <ignacio.a.r@hotmail.com>
2017-12-11 16:53:29 -0300

    avergaaaa

--- app/src/main/java/com/example/her_1/afinal/MainActivity.java
+++ app/src/main/java/com/example/her_1/afinal/MainActivity.java
@@ -8,7 +8,10 @@ import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,7 +20,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -35,9 +43,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG =  MainActivity.class.getCanonicalName();
 
-    public static final String fecha = null;
-    private int counter = 0;
     private boolean active = false;
+    private boolean json = false;
 
     /**Check internet conection*/
     public static ConnectivityManager connMgr;
@@ -71,14 +78,21 @@ public class MainActivity extends AppCompatActivity {
         intentManana = new Intent(MainActivity.this, MainActivityManana.class);
         intentManana.putStringArrayListExtra("datos", new ArrayList<String>());
 
-        request_partidos = new Request.Builder().url("http://192.168.0.3:8080/OlimpicRestServer/olimpic/getpartidos").build();
-        request_culturales = new Request.Builder().url("http://192.168.0.3:8080/OlimpicRestServer/olimpic/getculturales").build();
+        request_partidos = new Request.Builder().url("http://192.168.1.9:8080/OlimpicRestServer/olimpic/getpartidos").build();
+        request_culturales = new Request.Builder().url("http://192.168.1.9:8080/OlimpicRestServer/olimpic/getculturales").build();
+
+        SimpleDateFormat formater = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        Date auxDate = new Date();
+
+        String res = formater.format(auxDate);
+        text.setText(res);
 
         client.newCall(request_partidos).enqueue(new Callback() {
 
             @Override
             public void onFailure(Call call, IOException e) {
                 e.printStackTrace();
+                Log.d(TAG, "NO ANDAAAAAAAAAAAAAAAAAAAAAAA");
             }
 
             @Override
@@ -86,7 +100,12 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     String responseData = response.body().string();
                     jsonjArray_partidos = new JSONArray(responseData);
-                } catch (JSONException e) {e.printStackTrace();}
+                    synchronized (MainActivity.this) {
+                        active = true;
+                        MainActivity.this.notifyAll();
+                    }
+                    Log.d(TAG, "ANDAAAAAAAAAAAAAAAAAAAAAAA33333333");
+                } catch (JSONException e) {Log.d(TAG, "NO ANDAAAAAAAAAAAAAAAAAAAAAAA2");}
             }
         });
 
@@ -95,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call call, IOException e) {
                 e.printStackTrace();
+                Log.d(TAG, "NO ANDAAAAAAAAAAAAAAAAAAAAAAA");
             }
 
             @Override
@@ -102,7 +122,12 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     String responseData = response.body().string();
                     jsonjArray_culturales = new JSONArray(responseData);
-                } catch (JSONException e) {e.printStackTrace();}
+                    synchronized (MainActivity.this) {
+                        active = true;
+                        MainActivity.this.notifyAll();
+                    }
+                    Log.d(TAG, "ANDAAAAAAAAAAAAAAAAAAAAAAA33333333");
+                } catch (JSONException e) {Log.d(TAG, "NO ANDAAAAAAAAAAAAAAAAAAAAAAA2");}
             }
         });
 
@@ -110,16 +135,6 @@ public class MainActivity extends AppCompatActivity {
         connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         networkInfo = connMgr.getActiveNetworkInfo();
 
-        FloatingActionButton bSelect = (FloatingActionButton) findViewById(R.id.floatingActionButton);
-        bSelect.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                start();
-                Intent intent;
-                intent = new Intent(MainActivity.this, MainActivityCalendar.class);
-                startActivityForResult(intent, GET_RESULTC);
-            }
-        });
         FloatingActionButton bAyer = (FloatingActionButton) findViewById(R.id.b_ayer);
         bAyer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -144,25 +159,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intentManana, GET_RESULTM);
             }
         });
-        //this.start();
+        this.start();
     }
     public void start(){
         if (networkInfo != null && networkInfo.isConnected()) {
             /**Hilo*/
-            AsyncTask<?, ?, ?> asyncTask = new AsyncTask<Object, Evento, Object>(){
-
+            AsyncTask<?, ?, ?> asyncTask = new AsyncTask<Object, Evento, Object>() {
                 @Override
                 protected Object doInBackground(Object... params) {
 
-                        while (!active) {
-                            synchronized (MainActivity.this) {
-                                try {
-                                    MainActivity.this.wait();
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
+                    while (!active) {
+                        synchronized (MainActivity.this) {
+                            try {
+                                MainActivity.this.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
                             }
                         }
+                    }
 
                     intentAyer.getStringArrayListExtra("datos").clear();
                     intentHoy.getStringArrayListExtra("datos").clear();
@@ -210,13 +224,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                     active = false;
-                                //if (!active)
-                                    //break;
-                                //counter++;
-                                //
-                           // }
                     return null;
-                    //}
                 }
 
                 @Override
@@ -250,36 +258,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult ( int RequestCode, int ResultCode, Intent data){
         if (RequestCode == GET_RESULTA) {
-            if (ResultCode == RESULT_OK) {
-                //MainActivity.mensajeToast(this, "Retorna de AYER");
-            }else {
+            if (ResultCode != RESULT_OK) {
                 MainActivity.mensajeToast(this, "No hay conexion a internet");
             }
         }
         if (RequestCode == GET_RESULTH) {
-            if (ResultCode == RESULT_OK) {
-                //MainActivity.mensajeToast(this, "Retorna de HOY");
-            }else {
+            if (ResultCode != RESULT_OK) {
                 MainActivity.mensajeToast(this, "No hay conexion a internet");
             }
         }
         if (RequestCode == GET_RESULTM) {
-            if (ResultCode == RESULT_OK) {
-                //MainActivity.mensajeToast(this, "Retorna de MAÑANA");
-                //tv.setText(data.getStringExtra("result"));
-            }else {
-                MainActivity.mensajeToast(this, "No hay conexion a internet");
-            }
-        }
-        if (RequestCode == GET_RESULTC) {
-            if (ResultCode == RESULT_OK) {
-
-                text.setText(data.getStringExtra("result"));
-                synchronized (MainActivity.this) {
-                    active = true;
-                    MainActivity.this.notifyAll();
-                }
-            }else {
+            if (ResultCode != RESULT_OK) {
                 MainActivity.mensajeToast(this, "No hay conexion a internet");
             }
         }
@@ -288,191 +277,5 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(a, msj, Toast.LENGTH_SHORT);
         //toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
         toast.show();
-    }
-    public static  String getCulturales(){
-        return "[\n" +
-                "    {\n" +
-                "        \"id\":1,\n" +
-                "        \"puntos\":8,\n" +
-                "        \"actividad\":\"Pintura\",\n" +
-                "        \"facultad\":{\"id\":1,\"nombre\":\"Exactas\"},\n" +
-                "        \"fecha\":1512494151103,\n" +
-                "        \"lugar\":\"CCU\"\n" +
-                "    },{\n" +
-                "        \"id\":2,\n" +
-                "        \"puntos\":7,\n" +
-                "        \"actividad\":\"Pintura\",\n" +
-                "        \"facultad\":{\"id\":2,\"nombre\":\"Economicas\"},\n" +
-                "        \"fecha\":1512494151103,\n" +
-                "        \"lugar\":\"CCU\"\n" +
-                "    },{\n" +
-                "        \"id\":3,\n" +
-                "        \"puntos\":9,\n" +
-                "        \"actividad\":\"Musica\",\n" +
-                "        \"facultad\":{\"id\":2,\"nombre\":\"Economicas\"},\n" +
-                "        \"fecha\":1512494151103,\n" +
-                "        \"lugar\":\"CCU\"\n" +
-                "    },{\n" +
-                "        \"id\":4,\n" +
-                "        \"puntos\":0,\n" +
-                "        \"actividad\":\"Pintura\",\n" +
-                "        \"facultad\":{\"id\":3,\"nombre\":\"Humanas\"},\n" +
-                "        \"fecha\":1512580551103,\n" +
-                "        \"lugar\":\"CCU\"\n" +
-                "    },{\n" +
-                "        \"id\":5,\n" +
-                "        \"puntos\":0,\n" +
-                "        \"actividad\":\"Musica\",\n" +
-                "        \"facultad\":{\"id\":4,\"nombre\":\"Veterinarias\"},\n" +
-                "        \"fecha\":1512580551103,\n" +
-                "        \"lugar\":\"CCU\"\n" +
-                "    },{\n" +
-                "        \"id\":6,\n" +
-                "        \"puntos\":0,\n" +
-                "        \"actividad\":\"Pintura\",\n" +
-                "        \"facultad\":{\"id\":3,\"nombre\":\"Humanas\"},\n" +
-                "        \"fecha\":1512580551103,\n" +
-                "        \"lugar\":\"CCU\"\n" +
-                "    },{\n" +
-                "        \"id\":7,\n" +
-                "        \"puntos\":0,\n" +
-                "        \"actividad\":\"Escultura\",\n" +
-                "        \"facultad\":{\"id\":1,\"nombre\":\"Exactas\"},\n" +
-                "        \"fecha\":1512666951103,\"lugar\":\"CCU\"\n" +
-                "    },{\n" +
-                "        \"id\":8,\n" +
-                "        \"puntos\":0,\n" +
-                "        \"actividad\":\"Musica\",\n" +
-                "        \"facultad\":{\"id\":3,\"nombre\":\"Humanas\"},\n" +
-                "        \"fecha\":1512666951103,\n" +
-                "        \"lugar\":\"CCU\"\n" +
-                "    },{\n" +
-                "        \"id\":9,\n" +
-                "        \"puntos\":0,\n" +
-                "        \"actividad\":\"Pintura\",\n" +
-                "        \"facultad\":{\"id\":4,\"nombre\":\"Veterinarias\"},\n" +
-                "        \"fecha\":1512666951103,\n" +
-                "        \"lugar\":\"CCU\"\n" +
-                "    }\n" +
-                "]";
-    }
-    public static  String getPartidos(){
-        return "[\n" +
-                "    {\n" +
-                "        \"id\":1,\n" +
-                "        \"resultado\":\"87-67\",\n" +
-                "        \"deporte\":\"Basquet\",\n" +
-                "        \"facultad1\":{\n" +
-                "            \"id\":1,\n" +
-                "            \"nombre\":\"Exactas\"},\n" +
-                "        \"facultad2\":{\n" +
-                "            \"id\":2,\n" +
-                "            \"nombre\":\"Economicas\"},\n" +
-                "        \"fecha\":1512247864308,\n" +
-                "        \"lugar\":\"CCU\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"id\":2,\n" +
-                "        \"resultado\":\"3-2\",\n" +
-                "        \"deporte\":\"Ping Pong\",\n" +
-                "        \"facultad1\":{\n" +
-                "            \"id\":2,\n" +
-                "            \"nombre\":\"Economicas\"},\n" +
-                "        \"facultad2\":{\n" +
-                "            \"id\":3,\n" +
-                "            \"nombre\":\"Humanas\"},\n" +
-                "        \"fecha\":1512247864308,\n" +
-                "        \"lugar\":\"CCU\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"id\":3,\n" +
-                "        \"resultado\":\"2-3\",\n" +
-                "        \"deporte\":\"Voley Femenino\",\n" +
-                "        \"facultad1\":{\n" +
-                "            \"id\":2,\n" +
-                "            \"nombre\":\"Economicas\"},\n" +
-                "        \"facultad2\":{\n" +
-                "            \"id\":4,\n" +
-                "            \"nombre\":\"Veterinarias\"},\n" +
-                "        \"fecha\":1512247864308,\n" +
-                "        \"lugar\":\"CCU\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"id\":4,\n" +
-                "        \"resultado\":null,\n" +
-                "        \"deporte\":\"Futbol\",\n" +
-                "        \"facultad1\":{\n" +
-                "            \"id\":3,\n" +
-                "            \"nombre\":\"Humanas\"},\n" +
-                "        \"facultad2\":{\n" +
-                "                \"id\":4,\n" +
-                "                \"nombre\":\"Veterinarias\"},\n" +
-                "        \"fecha\":1512334264308,\n" +
-                "        \"lugar\":\"CCU\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"id\":5,\n" +
-                "        \"resultado\":null,\n" +
-                "        \"deporte\":\"Basquet\",\n" +
-                "        \"facultad1\":{\n" +
-                "            \"id\":4,\n" +
-                "            \"nombre\":\"Veterinarias\"},\n" +
-                "        \"facultad2\":{\n" +
-                "            \"id\":1,\n" +
-                "            \"nombre\":\"Exactas\"},\n" +
-                "        \"fecha\":1512334264308,\n" +
-                "        \"lugar\":\"CCU\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"id\":6,\n" +
-                "        \"resultado\":null,\n" +
-                "        \"deporte\":\"Handball\",\n" +
-                "        \"facultad1\":{\n" +
-                "            \"id\":3,\n" +
-                "            \"nombre\":\"Humanas\"},\n" +
-                "        \"facultad2\":{\n" +
-                "            \"id\":2,\n" +
-                "            \"nombre\":\"Economicas\"},\n" +
-                "        \"fecha\":1512334264308,\n" +
-                "        \"lugar\":\"CCU\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"id\":7,\n" +
-                "        \"resultado\":null,\n" +
-                "        \"deporte\":\"Fultbol\",\n" +
-                "        \"facultad1\":{\n" +
-                "            \"id\":1,\n" +
-                "            \"nombre\":\"Exactas\"},\n" +
-                "        \"facultad2\":{\n" +
-                "            \"id\":2,\n" +
-                "            \"nombre\":\"Economicas\"},\n" +
-                "        \"fecha\":1512420664308,\n" +
-                "        \"lugar\":\"Campus\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"id\":8,\n" +
-                "        \"resultado\":null,\n" +
-                "        \"deporte\":\"Fultbol\",\n" +
-                "        \"facultad1\":{\n" +
-                "            \"id\":3,\n" +
-                "            \"nombre\":\"Humanas\"},\n" +
-                "        \"facultad2\":{\n" +
-                "            \"id\":4,\n" +
-                "            \"nombre\":\"Veterinarias\"},\n" +
-                "        \"fecha\":1512420664308,\n" +
-                "        \"lugar\":\"Estadio\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"id\":9,\n" +
-                "        \"resultado\":null,\n" +
-                "        \"deporte\":\"Ping Pon\",\n" +
-                "        \"facultad1\":{\n" +
-                "            \"id\":1,\n" +
-                "            \"nombre\":\"Exactas\"},\n" +
-                "        \"facultad2\":null,\n" +
-                "        \"fecha\":1512420664308,\n" +
-                "        \"lugar\":\"CCU\"\n" +
-                "    }\n" +
-                "]";
     }
 }
